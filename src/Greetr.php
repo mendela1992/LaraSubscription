3a5fc256745a38281d5980d72b2fe0454a74d571
Nelson Mac <ndick4@gmail.com>
2020-07-09 19:17:20 +0200

    initial commit

---
+++ src/Greetr.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Mendela92\LaraSubscription;
+
+class Greetr
+{
+    public function greet(String $sName)
+    {
+        return 'Hi ' . $sName . '! How are you doing today?';
+    }
+}
